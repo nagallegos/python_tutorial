06ba4ada4ca75c75963211dd69d0a59180f11794
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-08-14 20:35:34 -0500

    Initial commit

---
+++ HelloWorld/app.py
@@ -0,0 +1,15 @@
+print("Hello World! :)")
+print("*" * 10)
+
+x = 1
+y = 2
+unit_price = 3
+print(x + y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+print(x + unit_price * y)
+print((x + unit_price) * y)
+print(x + unit_price * y)
